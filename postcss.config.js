82e6c451590649b7a42220e83ad6b53f62b2b5f6
Peter Huisman <peter.huisman@pggm.nl>
2024-03-18 11:19:10 +0100

    Remove postcss.config.js file

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-const postcssPresetEnv = require("postcss-preset-env");
-const postcssImport = require("postcss-import");
-module.exports = {
-  plugins: [
-    postcssPresetEnv({grid: true}),
-    postcssImport({path: ["./node_modules"]}),
-  ],
-};
